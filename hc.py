39e49581088dce0df4ec31c91463fb99c9af5d0e
Ippo <alla2634@edu.easj.dk>
2018-02-06 22:19:32 +0100

    Tying to do stuff

--- hc.py
+++ hc.py
@@ -1,19 +1,23 @@
 #!/bin/python
 
 # Standard library
+from base64 import b64encode
+from functools import partial
 import getopt
 import hashlib
 import sys
 
 
 # Third party library
-# try:
+#try:
 #     import numpy
 # except ImportError:
 #     print("Numpy not installed")
 
 
 # def main(argv):
+
+
 def main():
     input_file = 'testdata/text.dat'
     compare_value = '56e713c263a07f1c2bc4c0c6f44c46d7'
@@ -41,39 +45,45 @@ def main():
 
 
 def compare(input_hash, compare_hash, tha_hash=None):
-    with open(input_hash, "rb") as f:
-        a = hasher(f, tha_hash)
+    a = hasher(input_hash, tha_hash)
 
-        if tha_hash is None:
-            if a == compare_hash:
+    if tha_hash is None:
+        if a == compare_hash:
+            print("Hash is verified.")
+            exit(0)
+        else:
+            print("Hash is not verified.")
+    """elif tha_hash is not None:
+        for h in a:
+            if h == compare_hash:
                 print("Hash is verified.")
                 exit(0)
             else:
                 print("Hash is not verified.")
-        elif tha_hash is not None:
-            for h in a:
-                if h == compare_hash:
-                    print("Hash is verified.")
-                    exit(0)
-                else:
-                    print("Hash is not verified.")
+"""
 
-
-def hasher(value, mode):
-    try:
-        if mode == "md5":
-            return hashlib.md5(value)
+def hasher(value, mode): 
+    h = ''
+    if mode == "md5":
+        h = hashlib.md5()
+        """if mode == "md5":
+            h = hashlib.md5(value)
         elif mode == "sha1":
-            return hashlib.sha1(value)
+            h = hashlib.sha1(value)
         elif mode == "sha256":
-            return hashlib.sha256(value)
+            h = hashlib.sha256(value)
         elif mode == "sha512":
-            return hashlib.sha512(value)
+            h = hashlib.sha512(value)
         elif mode is None:
-            return [hashlib.md5(value), hashlib.sha1(value), hashlib.sha256(value), hashlib.sha512(value)]
-    except:
-        print("Wrong argument")
-        exit(2)
+            h = [hashlib.md5(value), hashlib.sha1(value), hashlib.sha256(value), hashlib.sha512(value)]
+        """
+        with open(value, "r") as f:
+            for l in f.readlines():
+                print(hashlib.md5(l.encode('utf-8')))
+
+#   except:
+#       print("Wrong argument")
+#        exit(2)
 
 
 def help_msg(mode):
@@ -83,7 +93,7 @@ def help_msg(mode):
     long_help_string = "{}\n\n\t-h, --hash: Select a specific hash algorithm. " \
                        "If a hash algorithm is not specified, will the program run all hashes, " \
                        "and compare each to the comare string. " \
-                       "\nCurrent algorithems supported: md5, SHA1, SHA256, SHA512"
+                       "\nCurrent algorithms supported: md5, SHA1, SHA256, SHA512"
 
     if mode == "s":
         print(short_help_string)
